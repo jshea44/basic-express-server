ca2cfe12658ea4f9451bfd85f9891cfac8d2789f
joshuashea <joshshea44@gmail.com>
2023-09-20 19:57:49 -0700

    various tweaks, added code to node.yml file

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -6,9 +6,9 @@ const request = supertest(app);
 
 describe('404 on a bad route', () => {
   test('Route should not exist, 404 expected', async () => {
-    let response = await request('/badroute');
+    let response = await request.get('/badroute');
     expect(response.status).toEqual(404);
-    expect(response.body).toEqual({ message: 'This is a bad route.' });
+    expect(response.body).toEqual({ message: 'Not Found' });
   });
 });
 
@@ -22,15 +22,15 @@ describe('404 on a bad method', () => {
 
 describe('500 on no name in query string', () => {
   test('No name in query string, 500 expected', async () => {
-    let response = await request('/person');
+    let response = await request.get('/person');
     expect(response.status).toEqual(500);
-    expect(response.body).toEqual({ message: 'No name in query string.' });
+    expect(response.body).toEqual({ message: 'Server Error' });
   });
 });
 
 describe('200 on name in query string', () => {
   test('Name is in the query string, 200 expected', async () => {
-    let response = await request('/person?name=fred');
+    let response = await request.get('/person?name=fred');
     expect(response.status).toEqual(200);
     expect(response.body).toEqual({ name: 'fred' });
   });
